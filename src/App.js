1af5d45779ac3bc35bf061cec54937f21d5eb86a
Darkstar <thisisdarkstar@duck.com>
2024-09-19 16:23:36 +0530

    template ready

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className='text-red-500'>Hello React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className='text-blue-500 text-2xl text-center'>This is the header</h1>
+        </div>
+    )
+}
+
+export default Navbar
